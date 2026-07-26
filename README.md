a8d97a5cfaa45e58729865d300e00c01f6ac7e59
Nickolas Morales <nickolamora@gmail.com>
2026-07-25 20:57:12 -0400

    Add Sampling functionality to the MCP

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Run the MCP server from VS Code through `.vscode/mcp.json`
 * Tools - Open up a Copilot Chat and you're able to use the tool  `#create-user` to create a user.
 * Resources - You can also see your resources like 'List all users' or 'retrieve user by id' so you can get it from the MCP Server and add it as context , etc.
 * Prompts - With Prompts, you can access them through the UI or through a `/` command in the Copilot chat i.e. `/mcp.test-msp-video-server.generate-fake..` to leverage a predetermined prompt with bits of data you need.
+* Sampling - can leverage #create-random-user to have the tool use the chat to generate a random user and do something with it
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import {StdioServerTransport} from "@modelcontextprotocol/sdk/server/stdio.js";
 import {z} from "zod";
 import fs from "node:fs/promises"
 import {ResourceTemplate} from "@modelcontextprotocol/sdk/server/mcp.js";
+import { CreateMessageResultSchema } from "@modelcontextprotocol/sdk/types.js";
 
 const server = new McpServer(
     {name: "test", version: "1.0.0"},
@@ -136,6 +137,65 @@ server.prompt("generate-fake-user", "Generate a fake user based on a given name"
         }
     }
 )
+
+// Configure Sampling for the server using tool
+server.tool(
+  "create-random-user",
+  "Create a random user with fake data",
+  {
+    title: "Create Random User",
+    readOnlyHint: false,
+    destructiveHint: false,
+    idempotentHint: false,
+    openWorldHint: true,
+  },
+  async () => {
+    const res = await server.server.request(
+      {
+        method: "sampling/createMessage",
+        params: {
+          messages: [
+            {
+              role: "user",
+              content: {
+                type: "text",
+                text: "Generate fake user data. The user should have a realistic name, email, address, and phone number. Return this data as a JSON object with no other text or formatter so it can be used with JSON.parse.",
+              },
+            },
+          ],
+          maxTokens: 1024,
+        },
+      },
+      CreateMessageResultSchema
+    )
+
+    if (res.content.type !== "text") {
+      return {
+        content: [{ type: "text", text: "Failed to generate user data" }],
+      }
+    }
+
+    try {
+      const fakeUser = JSON.parse(
+        res.content.text
+          .trim()
+          .replace(/^```json/, "")
+          .replace(/```$/, "")
+          .trim()
+      )
+
+      const id = await createUser(fakeUser)
+      return {
+        content: [{ type: "text", text: `User ${id} created successfully` }],
+      }
+    } catch {
+      return {
+        content: [{ type: "text", text: "Failed to generate user data" }],
+      }
+    }
+  }
+)
+
 
 async function createUser(user: {
     name: string,
